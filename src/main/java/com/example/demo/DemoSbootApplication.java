94238215d11f798299e5cf8f3c985b0fdb6d7614
venu <venugopal4355@gmail.com>
2022-04-28 15:31:30 +0530

    28 april

--- src/main/java/com/example/demo/DemoSbootApplication.java
+++ src/main/java/com/example/demo/DemoSbootApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan("com.example.demo")
 @SpringBootApplication
+@EnableJpaRepositories("com.example.demo")
 public class DemoSbootApplication {
 
 	public static void main(String[] args) {
